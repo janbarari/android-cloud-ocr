6991ea195ceb44944322779f935f1a4d661f88e6
Mehdi <workstation@Mehdis-iMac.local>
2021-06-30 18:33:42 +0430

    update final changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
-
-    implementation(project(":ocr"))
+    implementation("com.github.janbarari:android-cloud-ocr:v1.0.0-alpha")
 }
